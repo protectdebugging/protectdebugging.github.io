e92bd8e56c0d5875b714ecf15eea36fdb362bcc3
protectdebugging <protectdebugging@protonmail.com>
2024-10-01 19:19:37 +0300

    Update nodebugging.js

--- nodebugging.js
+++ nodebugging.js
@@ -1,8 +1,25 @@
 (function() {
-	function checkDebugger() {
+	var devtoolsOpen = false;
+	var checkInterval;
+
+	function blockPage() {
+		window.location.replace('https://protectdebugging.github.io/harika-icerik.html');
+		clearInterval(checkInterval);
 		debugger;
 	}
 
-	setInterval(checkDebugger,
-		0);
+	function detectDevTools() {
+		const start = Date.now();
+		debugger;
+		if (Date.now() - start > 100) {
+			devtoolsOpen = true;
+			blockPage();
+		}
+	}
+
+	checkInterval = setInterval(detectDevTools, 100);
+
+	window.addEventListener('load', function() {
+		detectDevTools();
+	});
 })();
