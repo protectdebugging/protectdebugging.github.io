5fe3a37cc920aa23b2a00a7fede445bde737c527
protectdebugging <protectdebugging@protonmail.com>
2024-10-27 12:09:06 +0300

    Update nodebugging.js

--- nodebugging.js
+++ nodebugging.js
@@ -10,14 +10,12 @@
 		maxMillisBeforeAckWhenClosed: 50,
 		moreAnnoyingDebuggerStatements: 1,
 		onDetectOpen: () => {
-			const revokeAllBlobUrls = () => {
-				const elements = document.querySelectorAll('script[src^="blob:"], link[href^="blob:"], img[src^="blob:"], video[src^="blob:"], audio[src^="blob:"]');
-				elements.forEach(el => {
-					if (el.src) URL.revokeObjectURL(el.src);
-					if (el.href) URL.revokeObjectURL(el.href);
-				});
-			};
-			revokeAllBlobUrls();
+			const videoElement = document.getElementById('videoPlayer');
+			if (videoElement && videoElement.src.startsWith('blob:')) {
+				URL.revokeObjectURL(videoElement.src);
+				videoElement.src = "";
+			}
+}
 			//window.location.replace("https://protectdebugging.github.io/harika-icerik");
 		},
 		onDetectClose: undefined,
