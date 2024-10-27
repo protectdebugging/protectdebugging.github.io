d6b0d9bfd258a57c70db096f8c588c474f2b8822
protectdebugging <protectdebugging@protonmail.com>
2024-10-27 11:09:48 +0300

    Update nodebugging.js

--- nodebugging.js
+++ nodebugging.js
@@ -6,7 +6,7 @@
 (() => {
 	/** @type {DevtoolsDetectorConfig} */
 	const config = {
-		pollingIntervalSeconds: 0.1,
+		pollingIntervalSeconds: 0.025,
 		maxMillisBeforeAckWhenClosed: 0,
 		moreAnnoyingDebuggerStatements: 0,
 		onDetectOpen: () => {
